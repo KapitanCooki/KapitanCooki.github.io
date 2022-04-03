53e8792d8d753da3b38035feb9199efcfa3b2573
KapitanCooki <101835515+KapitanCooki@users.noreply.github.com>
2022-04-03 22:44:09 +0200

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,26 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email ? $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $email_from = 'noreply@gmail.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "Username: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Messsage: $message.\n";
+
+
+    $to = "nizbourk@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .="Reply-To: $visitor_email \r\n"
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
+
+?>
